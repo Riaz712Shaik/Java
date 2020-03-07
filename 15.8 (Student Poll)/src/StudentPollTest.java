df93e4c536788690d46b2a4ee2e5844e4d363eda
Riaz712Shaik <57821582+Riaz712Shaik@users.noreply.github.com>
2020-03-06 20:20:21 -0800

    Add files via upload

---
+++ 15.8 (Student Poll)/src/StudentPollTest.java
@@ -0,0 +1,10 @@
+// Testing the StudentPoll class.
+
+public class StudentPollTest
+{
+public static void main( String args[] )
+{
+StudentPoll application = new StudentPoll();
+application.displayData();
+ } // end main
+ } // end class StudentPollTest
